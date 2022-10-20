b26ceafd59face057d62d630ab51183fb195a793
Varshini <varshuk01@gmail.com>
2022-10-20 08:21:06 +0530

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule,Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component'
+const appRoutes:Routes=[
+  {
 
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
